4769b66feef09a27ce1a145bcb8b317dd6bea050
sheizky <rivasfrancisco0128@gmail.com>
2022-09-13 20:40:00 -0300

    primer commit

---
+++ src/main/java/com/pruebaApi/proyectoGit/ProyectoGitApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaApi.proyectoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaApi/proyectoGit/ProyectoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaApi.proyectoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
