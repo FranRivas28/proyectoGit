308b7b90c3f0c40bd2ca9824e9900b6815ec9ca2
alejobarrios <alejobarrios00@gmail.com>
2022-09-25 19:28:05 -0300

    agregue controller

--- src/main/java/com/pruebaApi/proyectoGit/Controllers/UsuarioController.java
+++ src/main/java/com/pruebaApi/proyectoGit/Controllers/UsuarioController.java
@@ -1,4 +1,24 @@
 package com.pruebaApi.proyectoGit.Controllers;
 
+import java.util.ArrayList;
+import com.pruebaApi.proyectoGit.Models.UsuarioModel;
+import com.pruebaApi.proyectoGit.Services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> getUsuarios(){
+        return usuarioService.getUsuarios();
+    }
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
 }
